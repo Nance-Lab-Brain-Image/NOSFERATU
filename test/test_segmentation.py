633c702359330633b0adb8e8ae41478def9efeda
Krista Phommatha <kristaphommatha@gmail.com>
2025-02-27 15:48:02 -0800

    add split_image tests
    - added a test to check that split_image generates 4 images
    - added a test to check that split_image generates TypeError if a bad
      image format is passed
    - added a test to check that split_image generates a FileNotFoundError
      if a passed image does not exist
    - added a test to see that split_image generates a warning if a passed
      image has previously been split

--- test/test_segmentation.py
+++ test/test_segmentation.py
@@ -8,8 +8,31 @@ class TestSegmentation(unittest.TestCase):
     def test_bdreg(self):
         test = 0  # remove this line and replace with testing code
 
-    def test_split_images(self):
-        test = 0
+    def test_split_images_generates_4_images(self):
+        files_to_split_list = ['test_data/image1.png']  # check image type
+        split_image_list = split_images(files_to_split_list)
+        expected_image_output_list = ['image1_quada.npy',
+                                      'image1_quadb.npy',
+                                      'image1_quadc.npy',
+                                      'image1_quadd.npy']
+        for image in expected_image_output_list:
+            image = 'test_data/' + image
+            self.assertEqual((image, image.is_file()), (image, True))
+
+    def test_split_images_bad_images_quits(self):
+        files_to_split_list = ['test_data/image1.badformat']
+        with self.assertRaises(TypeError):
+            split_image_list = split_images(files_to_split_list)
+
+    def test_split_images_image_does_not_exist(self):
+        files_to_split_list = ['test_data/IDONTEXIST.png']
+        with self.assertRaises(FileNotFoundError):
+            split_image_list = split_images(files_to_split_list)
+
+    def test_split_images_image_already_split(self):
+        files_to_split_list = ['test_data/image1.png']
+        with self.assertWarns():
+            split_image_list_0 = split_images(files_to_split_list)
 
     def test_image_segmentation(self):
         test = 0
